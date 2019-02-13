a44cc4136eb68e55a8b97adf48e07d1d643c3ae7
Maudy <maudymorien@hotmail.nl>
2019-02-13 16:57:22 +0100

    did alot of stuff:

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -6,18 +6,50 @@ function submitComment() {
     const inputField = document.getElementById('name');
     const name = inputField.value;
     const textArea = document.getElementById('msg');
-    const msg = textArea.value;
+    var msg = textArea.value;
     const comment = document.createElement('section');
     const h3 = document.createElement('h3');
     const p = document.createElement('p');
     
+    if (doesNotPassAllValidations(name, msg)) {
+        return null;
+    }
+
     //adjust the elements we created
     h3.innerHTML = `${name} said:`;
     p.innerHTML = msg;
     comment.classList.add('comment');
     comment.appendChild(h3);
     comment.appendChild(p);
+    
 
-    console.log(comment);
+    //display the elements we created
+    const commentSection = document.getElementById('comments');
+    commentSection.appendChild(comment);
+   
+    //reset form values
+    inputField.value = null;
+    textArea.value = null;
 
-  }
+   
+}
+
+function doesNotPassAllValidations(name, msg) {
+    if (!name || !msg) {
+        alert('You forgot to fill in your name or message!')
+        return true;
+    }
+    if (msg.length > 280) {
+       alert('Your message is too long')
+       return true;
+    }
+        
+    else {
+        return false;
+    } 
+}
+
+function checkMsgLength(msg) {
+    var msg = document.getElementById('msg').value
+    alert(280 - msg.length + ' characters left')
+}
